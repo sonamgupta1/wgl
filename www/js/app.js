8ca16500bc42c5a65782f649056c10eea0ab6a70
sonam <sonam@headerlabs.com>
2015-05-09 15:59:01 +0530

    refrence pages and explandable list is ready

--- www/js/app.js
+++ www/js/app.js
@@ -4,128 +4,151 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
-
-//  .state('app.organization', {
-//    url: "/organization",
-//    views: {
-//      'menuContent': {
-//        templateUrl: "templates/organization.html",
-//        controller:"OrganizationCtrl"
-//      }
-//    }
-//  })
-
-//      .state('app.contact', {
-//          url: "/contact",
-//          views: {
-//              'menuContent': {
-//                  templateUrl: "templates/contact.html",
-//                  controller:"ContactCtrl"
-//              }
-//          }
-//      })
-
-  .state('app.token', {
-    url: "/token",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/token.html",
-          controller:"TokenCtrl"
-      }
-    }
-  })
-    .state('app.signin', {
-      url: "/signin",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/signin.html",
-          controller: 'SigninCtrl'
-        }
-      }
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
-      .state('app.add_project', {
-          url: "/add_project",
-          views: {
-              'menuContent': {
-                  templateUrl: "templates/add_project.html",
-                  controller: 'AddProjectCtrl'
-              }
-          }
-      })
-      .state('app.project_detail', {
-          url: "/project_detail",
-          views: {
-              'menuContent': {
-                  templateUrl: "templates/project_detail.html"
 
-              }
-          }
-      })
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-      .state('app.add_organization', {
-          url: "/add_organization",
-          views: {
-              'menuContent': {
-                  templateUrl: "templates/add_organization.html",
-                  controller: 'AddOrganizationCtrl'
-              }
-          }
-      })
-      .state('app.add_contact', {
-          url: "/add_contact",
-          views: {
-              'menuContent': {
-                  templateUrl: "templates/add_contact.html",
-                  controller: 'AddContactCtrl'
-              }
-          }
-      })
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'AppCtrl'
+            })
 
-      .state('app.contact_detail', {
-          url: "/contact_detail",
-          views: {
-              'menuContent': {
-                  templateUrl: "templates/contact_detail.html",
-                  controller: 'ContactDetailCtrl'
-              }
-          }
-      })
+            .state('app.token', {
+                url: "/token",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/token.html",
+                        controller: "TokenCtrl"
+                    }
+                }
+            })
+            .state('app.signin', {
+                url: "/signin",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/signin.html",
+                        controller: 'SigninCtrl'
+                    }
+                }
+            })
+
+            .state('app.projects', {
+                url: "/projects/",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/projects.html",
+                        controller: 'ProjectsCtrl'
+                    }
+                }
+            })
+
+            .state('app.project', {
+                url: "/project/:project_id",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/project.html",
+                        controller: 'ProjectCtrl'
+                    }
+                },
+                resolve: {
+                    project: function($stateParams) {
+                        console.log(JSON.stringify($stateParams))
+                    }
+                }
+            })
+
+            .state('app.add_project', {
+                url: "/project/:project_id/add_project",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/add_project.html",
+                        controller: 'ProjectCtrl'
+                    }
+                },
+                resolve: {
+                    project: function($stateParams) {
+                        console.log(JSON.stringify($stateParams))
+                    }
+                }
+            })
 
 
-      .state('app.projects', {
-    url: "/projects/",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/projects.html",
-        controller: 'ProjectCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/signin');
-});
+            .state('app.organization', {
+                url: "/project/:project_id/organization/:organization_id",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/organization.html",
+                        controller: 'OrganizationCtrl'
+                    }
+                },
+                resolve: {
+                    organization: function($stateParams) {
+                        console.log(JSON.stringify($stateParams))
+                    }
+                }
+            })
+
+            .state('app.add_organization', {
+                url: "/project/:project_id/organization/:organization_id/add_organization",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/add_organization.html",
+                        controller: 'OrganizationCtrl'
+                    }
+                },
+                resolve: {
+                    organization: function($stateParams) {
+                        console.log(JSON.stringify($stateParams))
+                    }
+                }
+            })
+
+            .state('app.contact', {
+                url: "/project/:project_id/organization/:organization_id/contact/:contact_id",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/contact.html",
+                        controller: 'ContactCtrl'
+                    }
+                },
+                resolve: {
+                    contact: function($stateParams) {
+                        console.log(JSON.stringify($stateParams))
+                    }
+                }
+            })
+            .state('app.add_contact', {
+                url: "/project/:project_id/organization/:organization_id/contact/:contact_id/add_contact",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/add_contact.html",
+                        controller: 'ContactCtrl'
+                    }
+                },
+                resolve: {
+                    contact: function($stateParams) {
+                        console.log(JSON.stringify($stateParams))
+                    }
+                }
+            })
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/signin');
+    });
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,195 @@
-angular.module('starter.services',[])
+angular.module('starter.services', [])
+    .factory('projectService', function () {
+
+        var allProjects = {
+            "projects": [
+                {
+                    "id": 111,
+                    "name": "Project 1",
+                    "organizations": [
+                        {
+                            "id": 1111,
+                            "name": "Project 1 Organization 1",
+                            "contacts": [
+                                {
+                                    "id": 755,
+                                    "name": "Project 1 Organization 1 Contact 1"
+                                },
+                                {
+                                    "id": 756,
+                                    "name": "Project 1 Organization 1 Contact 2"
+                                },
+                                {
+                                    "id": 757,
+                                    "name": "Project 1 Organization 1 Contact 3"
+                                }
+                            ]
+                        },
+                        {
+                            "id": 1112,
+                            "name": "Project 1 Organization 2",
+                            "contacts": [
+                                {
+                                    "id": 755,
+                                    "name": "Project 1 Organization 2 Contact 1"
+                                },
+                                {
+                                    "id": 756,
+                                    "name": "Project 1 Organization 2 Contact 2"
+                                },
+                                {
+                                    "id": 757,
+                                    "name": "Project 1 Organization 2 Contact 3"
+                                }
+                            ]
+                        },
+                        {
+                            "id": 1113,
+                            "name": "Project 1 Organization 3",
+                            "contacts": [
+                                {
+                                    "id": 755,
+                                    "name": "Project 1 Organization 3 Contact 1"
+                                },
+                                {
+                                    "id": 756,
+                                    "name": "Project 1 Organization 3 Contact 2"
+                                },
+                                {
+                                    "id": 757,
+                                    "name": "Project 1 Organization 3 Contact 3"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "id": 222,
+                    "name": "Project 2",
+                    "organizations": [
+                        {
+                            "id": 2221,
+                            "name": "Project 2 Organization 1",
+                            "contacts": [
+                                {
+                                    "id": 755,
+                                    "name": "Project 2 Organization 1 Contact 1"
+                                },
+                                {
+                                    "id": 756,
+                                    "name": "Project 2 Organization 1 Contact 2"
+                                },
+                                {
+                                    "id": 757,
+                                    "name": "Project 2 Organization 1 Contact 3"
+                                }
+                            ]
+                        },
+                        {
+                            "id": 2222,
+                            "name": "Project 2 Organization 2",
+                            "contacts": [
+                                {
+                                    "id": 755,
+                                    "name": "Project 2 Organization 2 Contact 1"
+                                },
+                                {
+                                    "id": 756,
+                                    "name": "Project 2 Organization 2 Contact 2"
+                                },
+                                {
+                                    "id": 757,
+                                    "name": "Project 2 Organization 2 Contact 3"
+                                }
+                            ]
+                        },
+                        {
+                            "id": 2223,
+                            "name": "Project 2 Organization 3",
+                            "contacts": [
+                                {
+                                    "id": 755,
+                                    "name": "Project 2 Organization 3 Contact 1"
+                                },
+                                {
+                                    "id": 756,
+                                    "name": "Project 2 Organization 3 Contact 2"
+                                },
+                                {
+                                    "id": 757,
+                                    "name": "Project 2 Organization 3 Contact 3"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "id": 333,
+                    "name": "Project 3",
+                    "organizations": [
+                        {
+                            "id": 3331,
+                            "name": "Project 3 Organization 1",
+                            "contacts": [
+                                {
+                                    "id": 755,
+                                    "name": "Project 3 Organization 1 Contact 1"
+                                },
+                                {
+                                    "id": 756,
+                                    "name": "Project 3 Organization 1 Contact 2"
+                                },
+                                {
+                                    "id": 757,
+                                    "name": "Project 3 Organization 1 Contact 3"
+                                }
+                            ]
+                        },
+                        {
+                            "id": 3332,
+                            "name": "Project 3 Organization 2",
+                            "contacts": [
+                                {
+                                    "id": 755,
+                                    "name": "Project 3 Organization 2 Contact 1"
+                                },
+                                {
+                                    "id": 756,
+                                    "name": "Project 3 Organization 2 Contact 2"
+                                },
+                                {
+                                    "id": 757,
+                                    "name": "Project 3 Organization 2 Contact 3"
+                                }
+                            ]
+                        },
+                        {
+                            "id": 3333,
+                            "name": "Project 3 Organization 3",
+                            "contacts": [
+                                {
+                                    "id": 755,
+                                    "name": "Project 3 Organization 3 Contact 1"
+                                },
+                                {
+                                    "id": 756,
+                                    "name": "Project 3 Organization 3 Contact 2"
+                                },
+                                {
+                                    "id": 757,
+                                    "name": "Project 3 Organization 3 Contact 3"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+
+        return {
+            all: allProjects
+        }
+    })
     .factory('RequestService', function () {
         var service = {
             signin: function () {
